0fad767f580e033c4f930d4b561220e5381e4695
Suryani <suryanilestari123@gmail.com>
2022-11-19 00:23:03 +0700

    fix bugs host value

--- src/handler.js
+++ src/handler.js
@@ -1,20 +1,15 @@
+/* eslint-disable linebreak-style */
 const { nanoid } = require('nanoid');
 const notes = require('./notes');
+
 const addNoteHandler = (request, h) => {
     const { title, tags, body } = request.payload;
-
     const id = nanoid(16);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
-
     const newNote = {
-        title,
-        tags,
-        body,
-        id,
-        createdAt,
-        updatedAt,
-    };
+      title, tags, body, id, createdAt, updatedAt,
+  };
 
     notes.push(newNote);
 
@@ -28,14 +23,49 @@ const addNoteHandler = (request, h) => {
                 noteId: id,
             },
         });
-        response.code(201);
-        return response;
-    }
+
+      response.code(201);
+
+      return response;
+  }
+
     const response = h.response({
         status: 'fail',
         message: 'Catatan gagal ditambahkan',
     });
+
     response.code(500);
     return response;
 };
-module.exports = { addNoteHandler };
+
+const getAllNotesHandler = () => ({
+    status: 'success',
+    data: {
+        notes,
+    },
+});
+
+const getNoteByIdHandler = (request, h) => {
+    const { id } = request.params;
+
+    const note = notes.filter((n) => n.id === Number(id))[0];
+
+    if (note !== undefined) {
+        return {
+            status: 'success',
+            data: {
+                note,
+            },
+        };
+    }
+
+    const response = h.response({
+        status: 'fail',
+        message: 'Catatan tidak ditemukan',
+    });
+
+    response.code(404);
+    return response;
+};
+
+module.exports = { addNoteHandler, getAllNotesHandler, getNoteByIdHandler };
--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes');
 
-const init = async() => {
+const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         routes: {
             cors: {
                 origin: ['*'],
@@ -13,7 +13,6 @@ const init = async() => {
     });
 
     server.route(routes);
-
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
 };
